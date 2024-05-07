380f6427de027de06330d9207d4c422de46921d7
trungducng0410 <ngtrungducyb@gmail.com>
2024-05-07 20:49:54 +1000

    init

---
+++ src/main/java/com/club/eliteclub/EliteclubApplication.java
@@ -0,0 +1,13 @@
+package com.club.eliteclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliteclubApplication.class, args);
+	}
+
+}
